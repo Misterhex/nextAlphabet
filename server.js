ad2e4aa2154713d1bbe60bf36e4812d569db3b4c
Yong Heng <yongheng@live.com.sg>
2014-10-09 23:44:29 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+
+var alphabetString = "#abcdefghijklmnopqrstuvwxyz";
+var alphabet = alphabetString.split('');
+var currentIndex = 0;
+
+console.log("initializing");
+
+function getNextAlphabet() {
+	console.log("getNextAlphabet()");
+	if (currentIndex >= alphabet.length) {
+		currentIndex = 0;
+	}
+	var returnVal = alphabet[currentIndex];
+	currentIndex++;
+	return returnVal;
+}
+
+
+http.createServer(function(req, res) {
+
+	console.log(req.url)
+
+	var next = getNextAlphabet();
+
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	res.write(next);
+	res.end();
+}).listen(8080);
